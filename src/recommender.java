d59e302bf969a78e85717f84060f17635bfdea02
samsonc <samsonc0514@gmail.com>
2021-08-20 23:36:48 -0400

    First commit

---
+++ src/recommender.java
@@ -0,0 +1,79 @@
+public class recommender{
+    private int id;
+    private String categories;
+    private String name;
+    private int popularity;
+    private int calories;
+    private int like;
+    private int nope;
+
+    public recommender(int id, String categories, String name, int popularity, int calories, int like, int nope){
+        this.id = id;
+        this.categories = categories;
+        this.name = name;
+        this.popularity = popularity;
+        this.calories = calories;
+        this.like = like;
+        this.nope = nope;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(int popularity) {
+        this.popularity = popularity;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public int getNope() {
+        return nope;
+    }
+
+    public void setNope(int nope) {
+        this.nope = nope;
+    }
+
+    
+
+}
---
+++ src/recommender.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+col_list = ["id", "categories", "name", "popularity", "calories", "like", "nope"]
+foodlist = pd.read_csv('../data/foodList.csv',  usecols=col_list)
+
+# foodlist.catergoies = foodlist.catergoies.str.lower()
+
+
+# print(foodlist["categories"])
+max_list = []
+nope_list = []
+
+for x in foodlist:
+    for item in foodlist[x]:
+        
+        if x == "like":
+            max_list.append(item)
+            print(max_list)
+            # find the maximum value from the list 
+            maxNum = max(max_list)
+            # print(maxNum)
+            #     
+        if x == "nope":
+            nope_list.append(item)
+            print(nope_list)
+            # find the min Nope value from the list 
+            minNope = min(nope_list)
+            print(minNope)
+               
+
+    
